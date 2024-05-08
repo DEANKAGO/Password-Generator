db97e3d37809a8678e21aad6769e076451a163a4
deankago <martokhago@gmail.com>
2024-05-08 05:25:59 -0400

    set states for password checkboxes

--- src/components/PasswordGenerator/PasswordGenerator.jsx
+++ src/components/PasswordGenerator/PasswordGenerator.jsx
@@ -9,6 +9,11 @@ import refreshIcon from '../../assets/icons/refresh.svg';
 import copyIcon from '../../assets/icons/copy.svg';
 
 export const PasswordGenerator = () => {
+  const [password, setPassword] = useState('');
+  const [useSymbols, setUseSymbols] = useState(true);
+  const [useNumbers, setUseNumbers] = useState(true);
+  const [useLowerCase, setUseLowerCase] = useState(true);
+  const [useUpperCase, setUseUpperCase] = useState(true);
   const [passwordLength, setPasswordLength] = useState(10);
   const onChangePasswordLength = (value) => {
     setPasswordLength(value);
@@ -17,61 +22,67 @@ export const PasswordGenerator = () => {
   return (
     <div className='container'>
       <div className='password-wrapper'>
-      <div className='gif'>
-        <img src={passwordGif} alt='Password Gif' />
-      </div>
-      <div className='tac'>
-        <h2 className='title'>PASSWORD GENERATOR</h2>
-        <p className='subtitle'>
-        Generate strong and random passwords to keep your accounts safe online!
-        </p>
-      </div>
-      <div className='password-input-wrapper'>
-        <div className='password-field'>
-          <input type='text' placeholder='your password' value='ABCDEFGHIJ' />
-          <img src={refreshIcon} alt='refresh the password' />
+        <div className='gif'>
+          <img src={passwordGif} alt='Password Gif' />
         </div>
-        <button className='copy-btn'>
-          <img src={copyIcon} alt='copy password' />
-          Copy
-        </button>
-      </div>
-      <span className='fw-500 health'>Weak</span>
-      <div className='slider'>
-        <div>
-          <label id='slider-label'>Password Length: </label>
-          <span>{passwordLength}</span>
+        <div className='tac'>
+          <h2 className='title'>PASSWORD GENERATOR</h2>
+          <p className='subtitle'>
+            Generate strong and random passwords to keep your accounts safe
+            online!
+          </p>
         </div>
-        <Slider
-          max={30}
-          min={5}
-          value={passwordLength}
-          onChange={onChangePasswordLength}
-          className='slider-style'
-        />
+        <div className='password-input-wrapper'>
+          <div className='password-field'>
+            <input type='text' placeholder='your password' value='ABCDEFGHIJ' />
+            <img src={refreshIcon} alt='refresh the password' />
+          </div>
+          <button className='copy-btn'>
+            <img src={copyIcon} alt='copy password' />
+            Copy
+          </button>
+        </div>
+        <span className='fw-500 health'>Weak</span>
+        <div className='slider'>
+          <div>
+            <label id='slider-label'>Password Length: </label>
+            <span>{passwordLength}</span>
+          </div>
+          <Slider
+            max={30}
+            min={5}
+            value={passwordLength}
+            onChange={onChangePasswordLength}
+            className='slider-style'
+          />
+        </div>
+        <div className='elements'>
+          <Checkbox
+            id='uppercase'
+            label='Uppercase'
+            checked={true}
+            name='upper'
+          />
+          <Checkbox
+            id='lowercase'
+            label='Lowercase'
+            checked={false}
+            name='lower'
+          />
+          <Checkbox
+            id='numbers'
+            label='Numbers'
+            checked={false}
+            name='numbers'
+          />
+          <Checkbox
+            id='special chars'
+            label='Special Characters'
+            checked={true}
+            name='specialChars'
+          />
+        </div>
       </div>
-      <div className='elements'>
-        <Checkbox
-          id='uppercase'
-          label='Uppercase'
-          checked={true}
-          name='upper'
-        />
-        <Checkbox
-          id='lowercase'
-          label='Lowercase'
-          checked={false}
-          name='lower'
-        />
-        <Checkbox id='numbers' label='Numbers' checked={false} name='numbers' />
-        <Checkbox
-          id='special chars'
-          label='Special Characters'
-          checked={true}
-          name='specialChars'
-        />
-      </div>
-    </div>
     </div>
   );
 };
